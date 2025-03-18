ae9cdc6e3028298d202d7d78bf359176cc7767d6
Hritvik Ramteke <kacchan@outlook.in>
2025-03-19 00:40:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ APP_ID = int(os.environ.get("APP_ID", "26788480"))
 API_HASH = os.environ.get("API_HASH", "858d65155253af8632221240c535c314")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002621268330"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002626895273"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "5163444566"))
